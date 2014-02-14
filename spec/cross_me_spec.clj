eb1c6bd101867dcf4c9e2051ccea68640b59841b
Kelly Steensma <kellysteensma@gmail.com>
2014-02-14 14:12:28 -0600

    Updating code to return a '.' instead of a double quote for blank spaces.  Adding more unit tests.

--- spec/cross_me_spec.clj
+++ spec/cross_me_spec.clj
@@ -3,41 +3,96 @@
             [cross-me    :refer :all]))
 
 (describe "cross me"
-  (context "builds a grid"
+  (describe "blank?"
+    (it "returns true if blank"
+      (should (blank? ".")))
+
+    (it "returns false if not blank"
+      (should-not (blank? "x"))))
+
+  (describe "not-blank?"
+    (it "returns true if not blank"
+      (should (not-blank? "x")))
+
+    (it "returns false if blank"
+      (should-not (not-blank? "."))))
+
+  (describe "#row-possibilities"
     (it "solves a row with only one extra space"
-      (let [p1 [nil "x" nil "x" "x"]
-            p2 ["x" nil nil "x" "x"]
-            p3 ["x" nil "x" "x" nil]
+      (let [p1 ["." "x" "." "x" "x"]
+            p2 ["x" "." "." "x" "x"]
+            p3 ["x" "." "x" "x" "."]
             ps (vector p1 p2 p3)]
       (should== ps (row-possibilities [nil nil nil nil nil] [1 2]))))
 
+    (it "solves a solid row"
+      (let [p1 ["x" "x" "x" "x" "x"]
+            ps (vector p1)]
+      (should== ps (row-possibilities [nil nil nil nil nil] [5]))))
+
+    (it "solves part of a solid row"
+      (let [p1 ["x" "x" "x" "x" "x" "." "." "."]
+            p2 ["." "x" "x" "x" "x" "x" "." "."]
+            p3 ["." "." "x" "x" "x" "x" "x" "."]
+            p4 ["." "." "." "x" "x" "x" "x" "x"]
+            ps (vector p1 p2 p3 p4)]
+      (should== ps (row-possibilities [nil nil nil nil nil nil nil nil] [5]))))
+
     (it "solves a row with two extra spaces"
-      (let [p1 [nil nil "x" "x" nil "x" "x" "x"]
-            p2 ["x" "x" nil "x" "x" "x" nil nil]
-            p3 [nil "x" "x" nil nil "x" "x" "x"]
-            p4 ["x" "x" nil nil nil "x" "x" "x"]
-            p5 [nil "x" "x" nil "x" "x" "x" nil]
-            p6 ["x" "x" nil nil "x" "x" "x" nil]
+      (let [p1 ["." "." "x" "x" "." "x" "x" "x"]
+            p2 ["x" "x" "." "x" "x" "x" "." "."]
+            p3 ["." "x" "x" "." "." "x" "x" "x"]
+            p4 ["x" "x" "." "." "." "x" "x" "x"]
+            p5 ["." "x" "x" "." "x" "x" "x" "."]
+            p6 ["x" "x" "." "." "x" "x" "x" "."]
             ps (vector p1 p2 p3 p4 p5 p6)]
       (should== ps (row-possibilities [nil nil nil nil nil nil nil nil] [2 3]))))
 
     (it "solves a row with two extra spaces"
+      (let [p1 ["." "x" "." "x" "x" "." "x"]
+            p2 ["x" "." "x" "x" "." "x" "."]
+            p3 ["x" "." "." "x" "x" "." "x"]
+            p4 ["x" "." "x" "x" "." "." "x"]
+            ps (vector p1 p2 p3 p4)]
+      (should== ps (row-possibilities [nil nil nil nil nil nil nil] [1 2 1]))))
+
+    (it "solves a row with two extra spaces when row has hints"
       (let [p1 [nil "x" nil "x" "x" nil "x"]
             p2 ["x" nil "x" "x" nil "x" nil]
             p3 ["x" nil nil "x" "x" nil "x"]
             p4 ["x" nil "x" "x" nil nil "x"]
             ps (vector p1 p2 p3 p4)]
-      (should== ps (row-possibilities [nil nil nil nil nil nil nil] [1 2 1]))))
+      (should== ps (row-possibilities [nil nil nil nil nil nil nil] [1 2 1])))))
 
-    (it "returns true when there are nils between each segment"
-      (should (nils-between-segments? [["x"] nil ["x" "x"] nil ["x"]])))
+    (describe "#segments-in-right-sequence?"
+      (it "returns true when the segments are in the right order"
+        (should (segments-in-right-sequence? '(["x"] "." ["x" "x"] ".") [["x"] ["x" "x"]])))
 
-    (it "returns true when there are nils between each segment"
-      (should (nils-between-segments? [["x"] nil nil ["x" "x"] nil nil ["x"]])))
+      (it "returns false when the segments are not in the right order"
+        (should-not (segments-in-right-sequence? '(["x"] "." ["x" "x"] ".") [["x" "x"] ["x"]]))))
 
-    (it "returns true when there are nils between each segment"
-      (should (nils-between-segments? [nil ["x"] nil ["x" "x"] nil ["x"] nil])))
+    (describe "#blanks-between-segments?"
+      (it "returns true when there are blanks between each segment"
+        (should (blanks-between-segments? [["x"] "." ["x" "x"] "." ["x"]])))
 
-    (it "returns true when there are nils between each segment"
-      (should-not (nils-between-segments? [["x"] ["x" "x"] nil ["x"]])))
-))
+      (it "returns true when there are blanks between each segment"
+        (should (blanks-between-segments? [["x"] "." "." ["x" "x"] "." "." ["x"]])))
+
+      (it "returns true when there are blanks between each segment"
+        (should (blanks-between-segments? ["." ["x"] "." ["x" "x"] "." ["x"] "."])))
+
+      (it "returns true when there are blanks between each segment"
+        (should-not (blanks-between-segments? [["x"] ["x" "x"] "." ["x"]])))
+
+      (it "returns true when there is only one solid segment"
+        (should (blanks-between-segments? [["x"]])))
+
+      (it "returns true when there is two solid segments"
+        (should (blanks-between-segments? [["x"] "." ["x"]])))
+
+      (it "returns true when there is two solid segments"
+        (should (blanks-between-segments? ["." ["x"] "."])))
+
+      (it "returns true when there is two solid segments"
+        (should (blanks-between-segments? ["." "." "." ["x"] "." "."]))))
+)
--- src/cross_me.clj
+++ src/cross_me.clj
@@ -1,39 +1,46 @@
 (ns cross-me
   (:require [clojure.math.combinatorics :as comb]))
 
-(def not-nil? (complement nil?))
+(def blank ".")
+(defn blank? [value] (= blank value))
+(defn not-blank? [value] (not= blank value))
 
 (defn first-ok? [row-possibility]
   (or
-    (and (not-nil? (first row-possibility))
-         (nil? (second row-possibility)))
-    (nil? (first row-possibility))))
+    (and (not-blank? (first row-possibility))
+         (= 1 (count row-possibility)))
+    (and (not-blank? (first row-possibility))
+         (blank? (second row-possibility)))
+    (blank? (first row-possibility))))
 
 (defn last-ok? [row-possibility]
   (or
-    (and (not-nil? (last row-possibility))
-         (nil? (nth row-possibility (- (count row-possibility) 2))))
-    (nil? (last row-possibility))))
+    (and (not-blank? (last row-possibility))
+         (= 1 (count row-possibility)))
+    (and (not-blank? (last row-possibility))
+         (blank? (nth row-possibility (- (count row-possibility) 2))))
+    (blank? (last row-possibility))))
 
 (defn middles-ok? [row-possibility]
   (loop [row-possibility row-possibility
          i 1]
-    (if (= i (dec (count row-possibility)))
+    (if (or (= 1 (count row-possibility))
+            (= i (dec (count row-possibility))))
       true
       (if (= false (or
-                     (nil? (nth row-possibility i))
-                     (and (nil? (nth row-possibility (inc i)))
-                          (nil? (nth row-possibility (dec i))))))
+                     (blank? (nth row-possibility i))
+                     (and (blank? (nth row-possibility (inc i)))
+                          (blank? (nth row-possibility (dec i))))))
         false
         (recur row-possibility (inc i))))))
 
-(defn nils-between-segments? [row-possibility]
+(defn blanks-between-segments? [row-possibility]
   (and (first-ok? row-possibility)
        (last-ok? row-possibility)
        (middles-ok? row-possibility)))
 
 (defn segments-in-right-sequence? [row-possibility segments]
-  (= segments (filter not-nil? row-possibility)))
+  (= segments (filter not-blank? row-possibility)))
 
 (defn flatten-nested-vectors [vector-with-nests]
   (map #(flatten %) vector-with-nests))
@@ -42,9 +49,10 @@
   (let [width (count row)
         filled-spaces (reduce + row-clue)
         segments (into [] (map #(into [] (repeat % "x")) row-clue))
-        empty-spaces (into [] (repeat (- width filled-spaces) nil))]
+        empty-spaces (into [] (repeat (- width filled-spaces) blank))]
     (flatten-nested-vectors
       (into []
-      (filter #(and (nils-between-segments? %) (segments-in-right-sequence? % segments))
+      (filter #(and (blanks-between-segments? %) (segments-in-right-sequence? % segments))
         (into [] (comb/permutations
           (concat segments empty-spaces))))))))
+
